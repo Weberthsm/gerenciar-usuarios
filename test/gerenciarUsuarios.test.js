e35652ca65b15bed71b1225ca0c409ee2c354c3c
weberthwsm <weberthwsm@gmail.com>
2025-09-09 23:16:33 -0300

    alterado lista de nomes de usuarios para tornarem-se objetos

--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -6,27 +6,41 @@ describe('Testar as funções de Gestão de Usuários', function(){
         //  Adicionar um novo nome no fianal da lista de usuários
         adicionarNovoUsuario({
         nome:  'Maria',
-        email: 'maria@dominio.com'
-    });
+        email: 'Maria@dominio.com'
+        });
   
    
-    // Retornar a lista de usuários
+        // Retornar a lista de usuários
         const listaDeUsuarios = retornarUsuarios();
+
         //comparar se o novo nome está no fim da lista de usuários
-   
-        expect(listaDeUsuarios.at(-1)).to.eql({
+        function equalIgnoreCase(obj1, obj2) {
+         return Object.keys(obj1).every(
+         key => obj1[key].toLowerCase() === obj2[key].toLowerCase()
+            );
+        }
+
+        expect(equalIgnoreCase(listaDeUsuarios.at(-1), {
+        nome: 'maria',
+        email: 'maria@dominio.com'
+    
+        })).to.be.true;
+
+
+       /*
+        expect(listaDeUsuarios.at(-1)).to.eql({   // eql para comparar objetos e equal para comparar propriedades
             nome:'Maria',
             email:'maria@dominio.com'
-   })
+        })
    
-   /*
+      */
+
+        /*
         expect(listaDeUsuarios.at(-1).nome).to.equal('Maria');
         expect(listaDeUsuarios.at(-1).email).to.equal('Maria@dominio.com');
-   */
+        */
    
     })
-
    
 
-   
 })
